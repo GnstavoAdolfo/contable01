dd7590bf6b78775ce6c645ecf310a1e40212c575
Gnstavo Adolfo <gnstavoadolfo@gmail.com>
2018-06-12 08:26:31 -0500

    Commit 001

--- db_contabilidad.sql
+++ db_contabilidad.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 12-06-2018 a las 08:38:03
--- Versión del servidor: 5.7.19
--- Versión de PHP: 5.6.31
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-06-2018 a las 13:23:49
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,16 +26,33 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `db_cliente`
 --
 
-DROP TABLE IF EXISTS `db_cliente`;
-CREATE TABLE IF NOT EXISTS `db_cliente` (
-  `1` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `db_cliente` (
+  `1` int(5) NOT NULL,
   `2` varchar(11) COLLATE utf8mb4_bin NOT NULL,
   `3` varchar(200) COLLATE utf8mb4_bin NOT NULL,
   `4` int(1) NOT NULL,
-  PRIMARY KEY (`1`)
+  `5` varchar(1) COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-COMMIT;
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `db_cliente`
+--
+ALTER TABLE `db_cliente`
+  ADD PRIMARY KEY (`1`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `db_cliente`
+--
+ALTER TABLE `db_cliente`
+  MODIFY `1` int(5) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
